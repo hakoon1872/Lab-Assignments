59fc20c01be5af1fd113ffd5a875fb4bbd1426f2
hakoon1872 <79046762+hakoon1872@users.noreply.github.com>
2025-02-27 01:08:07 +0500

    Add files via upload

---
+++ Assugnment01-SP24-BBA-086.py
@@ -0,0 +1,24 @@
+# Display several numbers
+print("Numbers:", 5, 10, 15, 20)
+
+# Solve and show the summation of 64 + 32
+sum1 = 64 + 32
+print("Sum of 64 + 32:", sum1)
+
+# Define variables x and y, and sum them
+x = 20
+y = 30
+sum2 = x + y
+print("Sum of x + y:", sum2)
+
+# Print the word 'Lucky' inside a variable
+s = "Lucky"
+print("The word inside s:", s)
+
+# Print the date in the format "Today is 2/2/2016"
+print("Today is 2/2/2016")
+
+# Ask for the user's phone number and display it
+phone_number = input("Enter your phone number: ")
+print("Your phone number is:", phone_number)
+
